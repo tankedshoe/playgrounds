372b6a2dc0ec8b0b74ad82dd3f5cfe0702e3feaa
tankedshoe <tankedshoe@gmail.com>
2017-09-25 08:35:57 -0600

    Playing around with variables.

--- intro.playground/Contents.swift
+++ intro.playground/Contents.swift
@@ -1,5 +1,31 @@
 //: Playground - noun: a place where people can play
 
 import UIKit
+//Explicit type declaration with no value
+var ramen :String
+//Implicit type declaration with a value to assign type.
+var soup = "ramen"
+ramen = soup
+print(ramen)
+print(soup)
+//ramen = 7
+var myAge :Int
+myAge = -89
+print(myAge)
+myAge = myAge * 3
+var someValue = 3.1415 * 56
+someValue = someValue * 231.1
+var isReal :Bool
+isReal = false
+isReal = true
+let myName :String
+myName = "Dane"
+print(myName)
+//myName = soup
+//Can't assign into a let once assigned
 
-var str = "Hello, playground"
+var junk :String = "test"
+print(junk)
+
+var junkDeux :Int = 7
+print(junkDeux + 2)
